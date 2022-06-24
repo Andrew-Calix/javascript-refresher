9953b55fc3d5c4a2a0bcba340db7edf53c443532
Andrew-Calix <andrewcalixb@gmail.com>
2022-06-24 08:15:26 +0530

    day 2

---
+++ Program to Generate a Random Number/app.js
@@ -0,0 +1,7 @@
+
+// Generates a random number between 10 and 100 in float
+const rnum = Math.random() * (100 - 10) + 10;
+// Rounds off to the nearest integer
+const inum = Math.floor(rnum);
+// Prints the value to console
+console.log(inum);
